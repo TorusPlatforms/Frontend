026c0864bbefac6d46cf681d9b539457e7f56217
StefanMerfy <112452566+StefanMerfy@users.noreply.github.com>
2023-12-03 22:06:56 -0800

    .

--- screens/createping/index.js
+++ screens/createping/index.js
@@ -11,12 +11,15 @@ export default function CreatePing() {
     return (
         
         <View style={{ flex: 1, paddingTop: 20, backgroundColor: "rgb(22, 23, 24)" }}>
+        
         <TouchableOpacity onPress={() => navigation.goBack()} style={{ padding: 10 }}>
           <Text style={{ fontSize: 16, color: "white", paddingLeft:10 }}>Cancel</Text>
         </TouchableOpacity>
+
         <View style={{ flex: 1, alignItems: 'center', justifyContent: 'flex-start' }}>
-          <Text style={{ paddingTop:20, fontWeight: "bold",fontSize: 30, color: "white" }}>New Ping</Text>
+          <Text style={{ paddingTop:20, fontWeight: "bold", fontSize: 30, color: "white" }}>New Ping</Text>
         </View>
+
       </View>
     )
 }
