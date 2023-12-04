5a5d42e3b0bbc535c5c0e0e4f4cfff59513d0200
StefanMerfy <112452566+StefanMerfy@users.noreply.github.com>
2023-12-03 18:08:25 -0800

    started create pings

--- screens/createping/index.js
+++ screens/createping/index.js
@@ -1,5 +1,5 @@
 import React, { useState, useRef, useEffect } from "react";
-import { View, Image, Text, Animated, Dimensions, Pressable, TextInput, Modal, FlatList, Button } from "react-native";
+import { View,TouchableOpacity, Image, Text, Animated, Dimensions, Pressable, TextInput, Modal, FlatList, Button } from "react-native";
 import { useNavigation } from "@react-navigation/native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
@@ -9,9 +9,14 @@ export default function CreatePing() {
     const navigation = useNavigation()
 
     return (
-        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text style={{ fontSize: 30 }}>This is a modal!</Text>
-        <Button onPress={() => navigation.goBack()} title="Dismiss" />
+        
+        <View style={{ flex: 1, paddingTop: 20, backgroundColor: "rgb(22, 23, 24)" }}>
+        <TouchableOpacity onPress={() => navigation.goBack()} style={{ padding: 10 }}>
+          <Text style={{ fontSize: 16, color: "white", paddingLeft:10 }}>Cancel</Text>
+        </TouchableOpacity>
+        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'flex-start' }}>
+          <Text style={{ paddingTop:20, fontWeight: "bold",fontSize: 30, color: "white" }}>New Ping</Text>
+        </View>
       </View>
     )
 }
