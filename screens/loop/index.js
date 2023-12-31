cb330b012f3f263a3b091a89600c0f0733e182ce
StefanMerfy <112452566+StefanMerfy@users.noreply.github.com>
2023-12-30 16:40:13 -0800

    loops notification button

--- screens/loop/index.js
+++ screens/loop/index.js
@@ -3,6 +3,7 @@ import { View, TouchableOpacity, Image, Text, TextInput, ScrollView } from "reac
 import { GiftedChat, Bubble } from 'react-native-gifted-chat';
 import styles from "./styles";
 import { useNavigation } from "@react-navigation/native";
+import Icon from '@expo/vector-icons/Ionicons';
 
 const exampleLoopData = {
     pfp: "https://cdn.discordapp.com/attachments/803748247402184714/822541056436207657/kobe_b.PNG?ex=658f138d&is=657c9e8d&hm=37b45449720e87fa714d5a991c90f7fac4abb55f6de14f63253cdbf2da0dd7a4&",
@@ -10,8 +11,11 @@ const exampleLoopData = {
     following: 128,
     followers: 259,
     description: "A place for Grants and Hoes to chill",
-    chats: ['Chat 1', 'Chat 2', 'Chat 3', 'Chat 4', 'Chat 5','Chat 6', 'Chat 7', 'Chat 8', 'Chat 9']
+    chats: ['Chat 1', 'Chat 2', 'Chat 3', 'Chat 4', 'Chat 5','Chat 6', 'Chat 7', 'Chat 8', 'Chat 9'],
+    recentAnnouncement: "Grant becomes world's first trillionaire after buying every single realfeel dumpad and selling them for billions each",
+    recentAnnouncementUser:"@stefan"
 }
+
 
 const ChatButton = ({ name, navigation }) => (
     <TouchableOpacity 
@@ -21,28 +25,54 @@ const ChatButton = ({ name, navigation }) => (
     </TouchableOpacity>
   );
 
+  
 
 const LoopsPage = () => {
     const navigation = useNavigation()
+    const [notifications, setNotifications] = useState(true);
+
     const goToLoop = () => {
         navigation.navigate('Profile'); 
       };
 
+    const toggleNotifications = () => {
+        setNotifications((prevNotifications) => !prevNotifications);
+      };
+
 
       return (
-        <View style={{ flex: 1, paddingTop: 20, backgroundColor: "rgb(22, 23, 24)" }}>
-          <TouchableOpacity onPress={goToLoop} style={{ padding: 10, marginTop: 30 }}>
-            <Text style={{ fontSize: 16, color: "white", paddingLeft: 10 }}>Back</Text>
-          </TouchableOpacity>
+        <View style={{  paddingTop: 20, backgroundColor: "rgb(22, 23, 24)" }}>
+        <View style={{ flexDirection: "row", justifyContent: "space-between" }}>
+            <TouchableOpacity onPress={goToLoop} style={{ padding: 10, marginTop: 30 }}>
+                <Text style={{ fontSize: 16, color: "white", paddingLeft: 10 }}>Back</Text>
+            </TouchableOpacity>
+
+            <TouchableOpacity onPress={toggleNotifications} style={{ padding: 10, marginTop: 30 }}>
+            {notifications ? (
+                <Icon name="notifications-outline" size={30} color="#ffffff" />
+            ) : (
+                <Icon name="notifications-off-outline" size={30} color="#ff0000" />
+            )}
+            </TouchableOpacity>
+        </View>
+         
           <Image style={{height:150, width:150, alignSelf:'center', borderRadius:200}}source={{uri: exampleLoopData.pfp}}/>
           <Text style={{color:"white", fontSize:30,marginTop:20, alignSelf:"center"}}>{exampleLoopData.displayName}</Text>
           <Text style={{color:"white", fontSize:20, alignSelf:"center"}}>{exampleLoopData.description}</Text>
-          
+
+            <View style={{backgroundColor: 'rgb(50,50,50)', alignSelf:"center", marginTop:50,width:"80%", borderRadius:20 }}>
             <TouchableOpacity 
             onPress={() => navigation.navigate("DirectMessage", {username: "Announcements"})}
-            style={{alignSelf: 'center', marginTop:25, backgroundColor: 'rgb(50,50,50)', paddingVertical: 15,paddingHorizontal: 50, borderRadius: 40,zIndex:0}}>
-            <Text style={{ color: 'white', fontSize: 20 }}>Announcements</Text>
+            style={{alignSelf: 'center',marginTop:10, backgroundColor: 'rgb(50,50,50)', paddingVertical: 15,paddingHorizontal: 50, borderRadius: 40,zIndex:0}}>
+            <Text style={{ color: 'white', fontSize: 20,textDecorationLine:"underline" }}>Announcements ></Text>
             </TouchableOpacity>
+            <View style={{paddingHorizontal:30, marginBottom:10}}>
+             <Text style={{color:"white"}}>{exampleLoopData.recentAnnouncementUser}</Text>
+             <Text style={{color:"white"}}>{exampleLoopData.recentAnnouncement}</Text>
+            </View>
+            
+            </View>
+            
 
             <ScrollView style={{marginTop:30, alignSelf:"center",alignContent:"center",maxHeight:300,borderBottomWidth:0,borderTopWidth:0,borderColor:'white', minWidth:"100%",}}>
             {exampleLoopData.chats.map((name, index) => (
