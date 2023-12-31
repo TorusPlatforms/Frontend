7e6db7a2b46e2ae1084102d6d7d352d649af674b
StefanMerfy <112452566+StefanMerfy@users.noreply.github.com>
2023-12-31 13:27:29 -0800

    set how many chats you want in a loop now, still need loop picture, animation

--- screens/createloop/index.js
+++ screens/createloop/index.js
@@ -1,67 +1,166 @@
 import React, { useState, useRef, useEffect } from "react";
-import { View, TouchableOpacity, Image, Text, TextInput, TouchableWithoutFeedback, Keyboard } from "react-native";
+import { View, TouchableOpacity, Text, TextInput, TouchableWithoutFeedback, Keyboard, ScrollView } from "react-native";
 import { useNavigation } from "@react-navigation/native";
-import { SafeAreaView } from "react-native-safe-area-context";
 import Icon from '@expo/vector-icons/Ionicons';
 
-import styles from "./styles";
-
-const exampleUserData = {
-  pfp: "https://cdn.discordapp.com/attachments/803748247402184714/822541056436207657/kobe_b.PNG?ex=658f138d&is=657c9e8d&hm=37b45449720e87fa714d5a991c90f7fac4abb55f6de14f63253cdbf2da0dd7a4&",
-  displayName: "Grant Hough",
-  username: "@granthough",
-  following: 128,
-  followers: 259,
-  description: "A pretty funny guy. Has a strong affinity for dogs. \n Stefan Murphy: 'The test is in'"
-}
-
 const CreatePing = () => {
-  const navigation = useNavigation();
-  const [textInputValue, setTextInputValue] = useState("");
-  const textInputRef = useRef(null);
-
-  useEffect(() => {
-    textInputRef.current.focus();
-  }, []);
-
-
-  const handleTapOutside = () => {
-    Keyboard.dismiss();
-  };
-
-  return (
-    <TouchableWithoutFeedback onPress={handleTapOutside}>
-    <View style={{ flex: 1, paddingTop: 20, backgroundColor: "rgb(22, 23, 24)" }}>
-        <View>
-          <TouchableOpacity onPress={() => navigation.goBack()} style={{ padding: 10 }}>
-            <Text style={{ fontSize: 16, color: "white", paddingLeft: 10 }}>Cancel</Text>
-          </TouchableOpacity>
+    const navigation = useNavigation();
+    const [nameInputValue, setNameInputValue] = useState("");
+    const [discInputValue, setDiscInputValue] = useState("");
+    const [chats, setChats] = useState([{ id: 1, value: "" }]);
   
-          <View style={{ alignItems: 'center', justifyContent: 'flex-start' }}>
-            <Text style={{ paddingTop: 0, fontWeight: "bold", fontSize: 30, color: "white" }}>Create New Loop</Text>
-          </View>
+    const textInputRefs = useRef(chats.reduce((acc, _, index) => {
+      acc[index] = React.createRef();
+      return acc;
+    }, {}));
   
-          <View style={{ flexDirection: "row", alignItems: "baseline" }}>
-            <Text style={{ color: "white", fontSize: 18, marginTop: 50, marginLeft: "25%" }}>Name:</Text>
-            <TextInput
-              ref={textInputRef}
-              style={{ marginLeft: 10, color: "white", fontSize: 18 }}
-              placeholder="Name"
-              multiline
-              numberOfLines={4}
-              maxLength={40}
-              placeholderTextColor="gray"
-              value={textInputValue}
-              onChangeText={(text) => setTextInputValue(text)}
-            />
-          </View>
-        </View>
+    useEffect(() => {
+      textInputRefs.current[0].current.focus();
+    }, []);
+  
+    const handleTapOutside = () => {
+      Keyboard.dismiss();
+    };
+  
+    const handleAddChat = () => {
+        const newId = chats.length + 1;
+        setChats([...chats, { id: newId, value: "" }]);
       
+        // Ensure the new ref is created
+        textInputRefs.current[newId - 1] = React.createRef();
+      
+        // Check if the ref is not null before focusing
+        if (textInputRefs.current[newId - 1].current) {
+          textInputRefs.current[newId - 1].current.focus();
+        }
+      };
 
+      const handleRemoveChat = () => {
+        if (chats.length > 1) {
+          const updatedChats = [...chats];
+          updatedChats.pop(); // Remove the last chat
+          setChats(updatedChats);
+        }
+      };
+  
+    const handleChatInputChange = (text, index) => {
+      const updatedChats = [...chats];
+      updatedChats[index].value = text;
+      setChats(updatedChats);
+    };
+  
+    return (
+      <TouchableWithoutFeedback onPress={handleTapOutside}>
+        <View style={{ flex: 1, paddingTop: 20, backgroundColor: "rgb(22, 23, 24)" }}>
+          <View>
+            <TouchableOpacity onPress={() => navigation.goBack()} style={{ padding: 10 }}>
+              <Text style={{ fontSize: 16, color: "white", paddingLeft: 10 }}>Cancel</Text>
+            </TouchableOpacity>
+  
+            <View style={{ alignItems: 'center', justifyContent: 'flex-start' }}>
+              <Text style={{ paddingTop: 0, fontWeight: "bold", fontSize: 30, color: "white" }}>Create New Loop</Text>
+            </View>
+  
+            <View style={{ flexDirection: "row", alignItems: "baseline" }}>
+              <Text style={{ color: "white", fontSize: 25, marginTop: 50, marginLeft: "5%" }}>Name:</Text>
+              <TextInput
+                ref={textInputRefs.current[0]}
+                style={{
+                  marginLeft: 10,
+                  color: "white",
+                  fontSize: 25,
+                  backgroundColor: "black",
+                  paddingBottom: 0,
+                  paddingRight: 70,
+                  paddingLeft: 5,
+                  maxWidth: 350,
+                  minWidth: 138
+                }}
+                placeholder="Name"
+                multiline
+                numberOfLines={4}
+                maxLength={40}
+                placeholderTextColor="gray"
+                value={nameInputValue}
+                onChangeText={(text) => setNameInputValue(text)}
+              />
+            </View>
+  
+            <View style={{ flexDirection: "row", alignItems: "baseline" }}>
+              <Text style={{ color: "white", fontSize: 25, marginTop: 25, marginLeft: "5%" }}>Description:</Text>
+              <TextInput
+                ref={textInputRefs.current[0]}
+                style={{
+                  marginLeft: 10,
+                  color: "white",
+                  fontSize: 10,
+                  backgroundColor: "black",
+                  paddingBottom: 0,
+                  paddingRight: 10,
+                  paddingLeft: 5,
+                  maxWidth: 220,
+                  minWidth: 135
+                }}
+                placeholder="What is your loop about?"
+                multiline
+                numberOfLines={4}
+                maxLength={100}
+                placeholderTextColor="gray"
+                value={discInputValue}
+                onChangeText={(text) => setDiscInputValue(text)}
+              />
+            </View>
+          </View>
+  
+          <View style={{ marginTop: 50 }}>
+            <Text style={{ color: "white", marginLeft: "5%", fontSize: 25 }}>Chats:</Text>
+                <ScrollView style={{maxHeight:450}}>
+
+                {chats.map((chat, index) => (
+              <View key={chat.id} style={{ flexDirection: "row", marginTop: "5%" }}>
+                <TextInput
+                  ref={textInputRefs.current[index]}
+                  style={{
+                    marginLeft: 10,
+                    color: "white",
+                    fontSize: 25,
+                    backgroundColor: "black",
+                    paddingBottom: 0,
+                    paddingRight: 70,
+                    paddingLeft: 0,
+                    maxWidth: 350,
+                    minWidth: 138,
+                    marginLeft: "5%"
+                  }}
+                  placeholder="Name"
+                  multiline
+                  numberOfLines={4}
+                  maxLength={40}
+                  placeholderTextColor="gray"
+                  value={chat.value}
+                  onChangeText={(text) => handleChatInputChange(text, index)}
+                />
+                {index === chats.length - 1 && (
+                    <View style={{flexDirection:"row"}}>
+                  <TouchableOpacity onPress={() => handleAddChat()}>
+                    <Icon name="add-circle-outline" size={30} color="#ffffff" style={{ marginLeft: 20 }} />
+                  </TouchableOpacity>
+
+                  <TouchableOpacity onPress={() => handleRemoveChat()}>
+                    <Icon name="remove-circle-outline" size={30} color="#ff0000" style={{ marginLeft: 20 }} />
+                  </TouchableOpacity>
+                  </View>
+                )}
+              </View>
+            ))}
+
+
+                </ScrollView>
+            
+          </View>
         </View>
-        </TouchableWithoutFeedback>
-
-  );
-};
-
-export default CreatePing;
+      </TouchableWithoutFeedback>
+    );
+  };
+  
+  export default CreatePing;
